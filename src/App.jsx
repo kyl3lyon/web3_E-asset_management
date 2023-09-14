cbc83d9adb6f7163a32ba1726e0b3496ff3f54fb
Kyle Lyon <kyle.m.lyon@gmail.com>
2023-09-14 07:38:30 +0000

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,13 @@ export default function App() {
             {/* Main Content */}
             <div className="w-3/4 p-8">
 
+                {/* Title */}
+                <h1 className="text-2xl font-bold mb-4">Digital Asset Upload</h1>
+                
+                {/* Sub Title */}
+                <h2 className="text-lg mb-6 text-gray-600">Secure and control your digital assets in decentralized storage with encryption and defined access policies.</h2>
+
+
                 {/* File Upload Section */}
                 <div className="border p-4 mb-6 bg-white rounded shadow-md">
                     <div className="bg-gray-200 p-4 text-center mb-2 rounded cursor-pointer hover:bg-gray-300">
@@ -26,6 +33,8 @@ export default function App() {
                         <button className="text-gray-800 ml-2 p-2 border rounded hover:bg-gray-300">Upload</button>
                     </div>
                 </div>
+
+                <p className="mb-4 text-gray-600">Set access controls for your uploaded content.</p>
 
                 {/* Access Controls */}
                 <div className="border p-4 mb-6 bg-white rounded shadow-md">
@@ -43,8 +52,10 @@ export default function App() {
                     </div>
                 </div>
 
+                <p className="mb-4 text-gray-600">Determine who can access your content.</p>
+
                 {/* Access Permissions */}
-                <div className="border p-4 bg-white rounded shadow-md">
+                <div className="border p-4 mb-6 bg-white rounded shadow-md">
                     <div className="flex mb-4 items-center">
                         <label className="mr-2 text-lg font-medium">Access:</label>
                         <select className="border p-2 rounded shadow-sm">
@@ -64,9 +75,20 @@ export default function App() {
                         </li>
                     </ul>
                 </div>
+                
+                <p className="mb-6 text-gray-600">Upload to the IPFS when ready.</p>
+
+                <button className=" bg-blue-600 text-white py-2 px-6 rounded hover:bg-blue-700">
+                    Upload
+                </button>
+
+                {/* Footer */}
+                <div className="mt-12 border-t pt-6">
+                    <p className="text-sm text-gray-600">© {new Date().getFullYear()} Distinct IP. All rights reserved. Powered By <a href="https://www.distinctip.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">DistinctIP.com</a></p>
+                </div>
+
 
             </div>
         </div>
     );
 }
-
